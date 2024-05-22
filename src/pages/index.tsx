a4df3e8dc857d39bba3837aa8af348637f744291
LorenzovandenDungen <2130766@talnet.nl>
2024-05-22 20:48:29 +0200

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,15 +41,7 @@ const Home: React.FC = () => {
           </div>
         </section>
       </main>
-      <footer className="bg-black text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <p>Ready for your next lesson?</p>
-          <div className="flex space-x-4">
-            <a href="#" className="hover:text-gray-400">Privacy Policy</a>
-            <a href="#" className="hover:text-gray-400">Terms & Conditions</a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
